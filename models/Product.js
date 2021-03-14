567c0b7d8def273d657793b31bbb1b4f19fbe8fa
marisanesmith <marisa@thisworldtraveled.com>
2021-03-14 10:54:33 -0400

    updated models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,36 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      // validates that the value is a decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      //validates that the value is numeric
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        // needs to reference the Category model's id
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    },
   },
   {
     sequelize,
