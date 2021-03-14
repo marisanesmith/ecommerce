7e04ebe619d31902ba310fe52e11ccde164b0be5
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-03-14 12:06:24 -0400

    updated product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
    // find all products
   // be sure to include its associated Category and Tag data
   try {
-    const getAllProducts = await Product.findAll({
+    const productData = await Product.findAll({
       include: [{ model: Category }, { model: Tag }],
       attributes: {
         include: [
@@ -23,16 +23,30 @@ router.get('/', (req, res) => {
         ],
       },
     });
-    res.status(200).json(getAllProducts);
+    res.status(200).json(productData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-// get one product
+// get one product -- check for accuracy
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category, through: Tag, as: 'product_tags' }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
