45c249607e834c340cdab8105e374b0a513d61e2
Marisa Nesmith <marisanesmith@Marisas-iMac.local>
2021-03-18 10:54:32 -0400

    updated category id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,7 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', async (req, res) => {
-  // find all categories -- check for accuracy
+  // find all categories 
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
@@ -18,10 +18,9 @@ router.get('/', async (req, res) => {
 });
 
 router.get('/:id', async (req, res) => {
-  // find one category by its `id` value -- check for accuracty
-  // be sure to include its associated Products
+  // find one category by its `id` value 
   try {
-    const categoryData = await Category.findAll({
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
     });
     if (!categoryData) {
@@ -36,7 +35,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  // create a new category -- check for accuracty
+  // create a new category 
   try {
     const categoryData = await Category.create(req.body);
     res.status(200).json(categoryData);
@@ -64,7 +63,7 @@ router.put('/:id', async (req, res) => {
 });
 
 router.delete('/:id', async (req, res) => {
-  // delete a category by its `id` value -- check for accuracy
+  // delete a category by its `id` value 
   try {
     const categoryData = await Category.destroy({
       where: {
